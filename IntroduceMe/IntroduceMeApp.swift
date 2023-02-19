ea8a2379dda876211b72d596018692de3f2f5fd6
YimKeul <leesjun29@gmail.com>
2023-02-20 00:33:29 +0900

    Initial Commit

---
+++ IntroduceMe/IntroduceMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroduceMeApp.swift
+//  IntroduceMe
+//
+//  Created by yimkeul on 2023/02/20.
+//
+
+import SwiftUI
+
+@main
+struct IntroduceMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
